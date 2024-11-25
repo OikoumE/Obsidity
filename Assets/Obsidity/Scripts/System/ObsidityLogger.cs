906e240f170561cab775243f8ee17b6ac3b4247c
Leif Hagen <leifchrlhagen@gmail.com>
2024-11-25 23:54:47 +0100

    added richtext on obsidityLogger

--- Assets/Obsidity/Scripts/System/ObsidityLogger.cs
+++ Assets/Obsidity/Scripts/System/ObsidityLogger.cs
@@ -6,16 +6,16 @@ public static class ObsidityLogger
 
     public static void LogErr(string err, Object ctx = null)
     {
-        Debug.LogError(_pre + err, ctx);
+        Debug.LogError($"<color=red>{_pre}</color>" + err, ctx);
     }
 
     public static void LogWrn(string wrn, Object ctx = null)
     {
-        Debug.LogWarning(_pre + wrn, ctx);
+        Debug.LogWarning($"<color=yellow>{_pre}</color>" + wrn, ctx);
     }
 
     public static void Log(string s, Object ctx = null)
     {
-        Debug.Log(_pre + s, ctx);
+        Debug.Log($"<color=green>{_pre}</color>" + s, ctx);
     }
 }
